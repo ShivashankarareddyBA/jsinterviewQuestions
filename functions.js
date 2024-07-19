483ef44a7ee490ab121595ce89d730bca0f4a493
shivashankarareddy <ssr2tech@gmail.com>
2024-07-19 19:26:07 +0530

    js functions

---
+++ functions.js
@@ -0,0 +1,17 @@
+
+
+//anonynoums functions
+// function (){
+    
+// } => it will throw an syntax error function statement must be named
+
+//function statement / declaration  
+function a (){
+    console.log("Wellcome to JavaScript");
+}
+a();
+//function expression 
+
+var b= function(){
+console.log("b is called")}
+b();
