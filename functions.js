9220c10075f931f4889ac8154a3c36d3046b6c15
shivashankarareddy <ssr2tech@gmail.com>
2024-07-19 19:47:34 +0530

    updated with difference between arguments and parameters

--- functions.js
+++ functions.js
@@ -15,3 +15,14 @@ a();
 var b= function(){
 console.log("b is called")}
 b();
+
+//difference between arguments and parameters
+//when you passing an identifier in to a function is called parameters
+//when you passing values into functions is calle arguments 
+
+const c = function(parm1, parm2){
+    console.log("c is called");
+    console.log("parm1:", parm1);
+    console.log("parm2:", parm2);
+}
+c(1, 2);
