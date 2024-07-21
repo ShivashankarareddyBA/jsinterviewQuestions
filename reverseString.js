038e72362c98dca4c74ac9bee8f45c12eb03c80d
shivashankarareddy <ssr2tech@gmail.com>
2024-07-21 20:32:00 +0530

    Another tricky interview question  has been added

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+let str="HANUMAN";
+console.log(str.split("").reverse().join(""));
+
+let str1 = "Hello, World!";
+let firstChar = str1[0]; // 'H'
+let fifthChar = str1[4]; // 'o'
+console.log(firstChar);
+console.log(fifthChar);
+
+
+//////////////////////
