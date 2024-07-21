bf4b30f41fcc2df398d717850cff83680e7f8938
shivashankarareddy <ssr2tech@gmail.com>
2024-07-21 21:23:55 +0530

    Another tricky interview question  has been added

--- reverseString.js
+++ reverseString.js
@@ -8,4 +8,17 @@ console.log(firstChar);
 console.log(fifthChar);
 
 
-//////////////////////
+// js every method
+const amcCollage=[{roll:7, marks:60},
+    {roll:5, marks:90},
+    {roll:2, marks:50},
+    {roll:10, marks:80}];
+    
+    // check if all students are passed
+    // if all students are passed then print true else false
+    
+    const passing_marks=35;
+    const isAllPass=amcCollage.every((student)=>{
+    return student.marks >= passing_marks;
+     }) ;
+    console.log (isAllPass);
