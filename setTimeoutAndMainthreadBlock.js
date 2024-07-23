78aee365c49f0e22f68b94c1c14dc35423e7bf2c
shivashankarareddy <ssr2tech@gmail.com>
2024-07-23 09:08:23 +0530

    calcuating the area circumference, diameter

--- setTimeoutAndMainthreadBlock.js
+++ setTimeoutAndMainthreadBlock.js
@@ -1,5 +1,5 @@
 console.log("start");
-
+// the call back function must have to register somewhere in the webAPI calls wheather it is an 0 milisecond also same process it will not exicute immediatly
 setTimeout(() => {
     console.log("cb");
 }, 500);
