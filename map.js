246b15f11e871241273bfe0b74cf585e829da22a
shivashankarareddy <ssr2tech@gmail.com>
2024-07-24 15:38:20 +0530

    map example

---
+++ map.js
@@ -0,0 +1,26 @@
+// const arr =[1,3,5,8,9];
+//we are using map when want to create newresulting array .
+
+const arr = [3,6,9,5];
+
+function double (x){
+    return x*2;
+}
+
+const output = arr.map(double);
+console.log(output);
+// triple
+function triple(x){
+    return x*3;
+}
+const output1 = arr.map(triple);
+
+console.log(triple);
+
+//transformer to binary 
+
+function binary(x){
+    return arr.toString(2);
+}
+
+const output2= arr.map(binary);
